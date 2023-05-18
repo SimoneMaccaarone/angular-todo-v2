cd8c7fa4e13b6d49e78d3afbdf5a77b81c4d68e4
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-05-18 13:17:14 +0200

    cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
+
+import { AppComponent } from './app.component';
 import { TodoAddComponent } from './components/todo-add/todo-add.component';
 import { TodoDetailComponent } from './components/todo-detail/todo-detail.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './footer/footer.component';
 
 
@@ -19,11 +23,16 @@ import { FooterComponent } from './footer/footer.component';
     TodoAddComponent,
     TodoDetailComponent,
     FooterComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
 
   ],
   providers: [],
